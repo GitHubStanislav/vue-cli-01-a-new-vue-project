b32528b2088244a52fd01156b7c649c1d4f813c1
Stanislav <spiridonov.kyiv@gmail.com>
2023-03-17 00:02:42 +0200

    Start : props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
+  <ul>
+<learning-resource
+    v-for="res in storedResources" :key="res.id"
+    :title="res.tittle"
+    :link="res.link"
+    :description="res.description"
+>
 
+</learning-resource>
+  </ul>
 </template>
 
 <script>
 
 
-export default {
-  components: {
 
-  },
+
+import LearningResource from "@/components/learning-resources/LearningResource.vue";
+
+export default {
+  components: {LearningResource},
   data() {
     return {
-
+      storedResources: [
+        {
+          id: 'official-guide',
+          tittle:'Official-Guide',
+          description:'The official Vue.js documentation.',
+          link:'https://vuejs.org/'
+        },
+        {
+          id: 'google',
+          tittle:'Google',
+          description:'Learn to google...',
+          link:'https://google.org/'
+        }
+      ]
     };
   },
 };
