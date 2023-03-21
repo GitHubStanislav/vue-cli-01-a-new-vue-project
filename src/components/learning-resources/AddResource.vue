50d5d64390301c79d749843c2da3776fa2864659
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-03-21 11:56:01 +0200

    +

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+<template #default>
+  <p>Unfortunately, at least one input value is invalid</p>
+  <p>Please check all inputs </p>
+</template>
+    <template #actions>
+      <buttons-style @click="confirmError">Okay</buttons-style>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -34,21 +43,29 @@
 </template>
 
 <script>
-
-
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+
+    }
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value
       const enteredDescription = this.$refs.descInput.value
       const enteredURL = this.$refs.linkInput.value
 
-      if(enteredTitle.trim()==='' || enteredDescription.trim()==='' || enteredURL.trim() ===''){
-return
+      if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredURL.trim() === '') {
+        this.inputIsInvalid = true
+        return
       }
 
       this.addResource(enteredTitle, enteredDescription, enteredURL)
+    },
+    confirmError(){
+      this.inputIsInvalid = false
     }
   },
   name: "AddResource",
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import {createApp} from 'vue';
 import App from "@/App.vue";
 import ButtonsStyle from "@/components/UI/ButtonsStyle.vue";
 import BaseCard from "@/components/UI/BaseCard.vue";
+import BaseDialog from "@/components/UI/BaseDialog.vue";
 
 const app = createApp(App)
 
 app.component('buttons-style', ButtonsStyle)
 app.component('base-card', BaseCard)
+app.component('base-dialog',BaseDialog)
 app.mount('#app');
