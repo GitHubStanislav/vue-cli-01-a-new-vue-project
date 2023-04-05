064cf0611eb295604c5d35f0d01f31f3ca3d96cf
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-05 12:17:07 +0300

    +

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory} from 'vue-router'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import TeamsList from "@/components/teams/TeamsList.vue";
+import UsersList from "@/components/users/UsersList.vue";
 
-import App from './App.vue';
+import App from "./App.vue";
 
 const router = createRouter({
-    routes: [
-        {}
-    ],
-    history: createWebHistory()
-})
-const app = createApp(App)
+  history: createWebHistory(),
+  routes: [
+    { path: "/teams", component: TeamsList },
+    { path: "/users", component: UsersList },
+  ],
+});
+const app = createApp(App);
+app.use(router);
 
-app.mount('#app');
+app.mount("#app");
