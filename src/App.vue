02518e7b5ba073eec3c61d4bcb3d4571278cca7b
Stanislav <spiridonov.kyiv@gmail.com>
2023-04-09 22:48:48 +0300

    +

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
-  <div class="wrapper">
-    <div>
-      like: <strong>{{ like }} </strong>
-    </div>
-    <div>
-      Dislike: <strong>{{ dislike }}</strong>
-    </div>
-      <button>Like</button>
-      <button>Dislike</button>
+  <div class="post">
+
   </div>
 </template>
 
@@ -20,80 +13,17 @@ export default {
       dislike: 0,
     };
   },
-  methods: {
-    addLike() {},
-    disLike() {},
-  },
+  methods: {},
 };
 </script>
 
 <style>
 * {
   box-sizing: border-box;
-}
-
-html {
-  font-family: "Jost", sans-serif;
-}
-
-body {
-  margin: 0;
-}
-
-.wrapper{
-    margin: 5px auto;
-    border: 2px solid coral;
-}
-
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app ul {
   margin: 0;
   padding: 0;
-  list-style: none;
 }
-
-#app li {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-
-#app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+.post{
+    
 }
 </style>
