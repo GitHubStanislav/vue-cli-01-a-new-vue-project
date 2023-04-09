49e630335d60593e583278c9629d8e5bcbefad39
Stanislav <spiridonov.kyiv@gmail.com>
2023-04-09 23:24:15 +0300

    add inputs with button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="post">
-
+ <div class="app">
+  <form>
+    <h4>Creating post</h4>
+    <input class="input" type="text" placeholder="Add some post:)" />
+    <input class="input" type="text" placeholder="Description" />
+    <button class="btn">Create post</button>
+  </form>
+  <div class="post" v-for="post in posts" :key="post.id">
+    <div>Name {{ post.title }}</div>
+    <div>Description {{ post.body }}</div>
   </div>
+ </div>
 </template>
 
 <script>
@@ -9,8 +18,23 @@ export default {
   components: {},
   data() {
     return {
-      like: 0,
-      dislike: 0,
+      posts: [
+        {
+          id: 1,
+          title: "JavaScript",
+          body: "Description 1 ",
+        },
+        {
+          id: 2,
+          title: "Java",
+          body: "Description 2",
+        },
+        {
+          id: 2,
+          title: "C++",
+          body: "Description 3",
+        },
+      ],
     };
   },
   methods: {},
@@ -23,7 +47,39 @@ export default {
   margin: 0;
   padding: 0;
 }
-.post{
-    
+.app{
+    padding: 20px;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    background: lightsteelblue;
+    color: black;
+    border: 1px solid teal;
+    padding: 2px;
+}
+.btn:hover{
+    background-color: coral;
+}
+
+.post {
+  margin: 1rem 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 12px;
+  padding: 1rem;
+}
+.input {
+  width: 100%;
+  border: 1px solid #9e9e9e;
+  color: #000000;
+  padding: 3px 10px;
+  margin-top: 10px;
+  font-size: 11px;
+  background: #fff;
+
 }
 </style>
